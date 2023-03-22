7bb4f1b9da13d723f9066e90eada6c009d86be33
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2023-03-22 17:44:01 +0700

    memasukkan perintah panjang, lebar, tinggi, alas,

--- onlineklasspd/onlineklasspd.cpp
+++ onlineklasspd/onlineklasspd.cpp
@@ -13,3 +13,23 @@ float Luaslingkaran(float r) {
     return 3.14 * r * r;
 }
 
+int main() {
+
+    float panjang, lebar, tinggi, jejari, alas;
+    int pilihan;
+
+
+
+    do
+    {
+        cout << "\n\n---------------------" << endl;
+        cout << "-------M E N U-------" << endl;
+        cout << "---------------------" << endl;
+
+        cout << "1. Luas Persegipanjang" << endl;
+        cout << "2. Luas Segitiga" << endl;
+        cout << "3. Luas Lingkaran" << endl;
+        cout << "4. Exit" << endl;
+
+        cout << "Masukan pilihan : ";
+        cin >> pilihan;
